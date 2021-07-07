c6d1b52525e3070bcc3cce0f303a453cf72f96c6
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2021-07-08 00:12:30 +0200

    adding events

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 function App(props) {
-  const taskList = props.tasks.map(task => <Todo id={task.id} 
-    name={task.name} completed={task.completed} key={task.id} />);
+  const [tasks, setTasks] = useState(props.tasks);
+  function addTask(name){
+    alert(name);
+  }
+
+  const taskList = props.tasks.map(task => (
+  <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />)
+  );
+  
   return (
     <div className="todoapp stack-large">
       <h1>ToDo App</h1>
-      <Form />
+      <Form addTask = {addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <button type="button" className="btn toggle-btn" aria-pressed="false">
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 
-export default function Form(){
+
+export default function Form(props){
+  const [name, setName] = useState('');
+  function handleChange(e){
+    setName(e.target.value);
+  }
+  
+  function handleSubmit(e){
+    e.preventDefault();
+    props.addTask(name);
+    setName("");
+  }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
         <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label__lg">
             What needs to be done?
           </label>
         </h2>
         <input
+          onChange={handleChange}
+          value={name}
           type="text"
           id="new-todo-input"
           className="input input__lg"

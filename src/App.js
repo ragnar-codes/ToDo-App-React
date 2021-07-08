ed6d54d794387c15c1c16cf54325c5311ce3e4a8
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2021-07-08 10:51:27 +0200

    adding task

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, {useState} from "react";
+import {nanoid} from "nanoid";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
   function addTask(name){
-    alert(name);
+    const newTask = {id: 'todo-' + nanoid(), name: name, completed: false};
+    setTasks([...tasks, newTask]);
+  }
+
+  function toggleTaskCompleted(id){
+    const updatedTasks = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+      // use object spread to make a new object
+      // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+    }
+    return task;
+  });
+    setTasks(updatedTasks);
+  }
   }
 
   const taskList = tasks.map(task => (
-  <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />)
+  <Todo id={task.id} name={task.name} 
+  completed={task.completed} key={task.id} toggleTaskCompleted={toggleTaskCompleted} />)
   );
-  
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
     <div className="todoapp stack-large">
       <h1>ToDo App</h1>
@@ -30,7 +48,7 @@ function App(props) {
         </button>
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {headingText}
       </h2>
       <ul
         role="list"
